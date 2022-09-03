4b8f70c8ace6dccab04ae58853125b3347e0ed2b
yudgxe <yudgxe@gmail.com>
2022-09-03 09:11:05 +0300

    small rework

--- 13.go
+++ 13.go
@@ -2,10 +2,8 @@ package main
 
 import "fmt"
 
-func swap(a, b *int) {
-	*a = *a - *b // a = 9 - 4 = 5
-	*b = *a + *b // b = 5 + 4 = 9
-	*a = *b - *a // a = 9 - 5
+func swap[T any](a, b *T) {
+	*a, *b = *b, *a
 }
 
 func main() {
@@ -13,7 +11,7 @@ func main() {
 	b := 5
 
 	swap(&a, &b)
-
+	
 	fmt.Println(a)
 	fmt.Println(b)
 }
